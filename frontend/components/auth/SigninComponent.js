db3ad693e99b6bf22b1089608aa71f18e933eb1f
Lebeil <liedel75@gmail.com>
2021-01-05 22:18:12 +0100

    add signin & signout link

---
+++ frontend/components/auth/SigninComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SigninComponent = () => {
+    return (
+        <div>
+            <h2>Signin</h2>
+        </div>
+    );
+};
+
+export default SigninComponent;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,15 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import Link from 'next/link';
 
-const Index = ()=> {
-    return(
-            <Layout>
-                <p>header</p>
-                <h2>Index page</h2>
-                <p>footer</p>
-            </Layout>
-        )
-}
+const Index = () => {
+    return (
+        <Layout>
+            <h2>Index page</h2>
+            <Link href="/signup">
+                <a>Signup</a>
+            </Link>
+        </Layout>
+    );
+};
 
-export default Index
+export default Index;
