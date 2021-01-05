a7314934c24d79bdd5e5622191f2c3a28c741716
Lebeil <liedel75@gmail.com>
2021-01-05 17:25:14 +0100

    add authentication

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 //link routes
 const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
 
 //app
 const app = express();
@@ -28,7 +29,7 @@ if(process.env.NODE_ENV === 'development') {
 
 //routes middlewares
 app.use('/api', blogRoutes)
-
+app.use('/api', authRoutes)
 
 //port
 const port = process.env.PORT || 8000
