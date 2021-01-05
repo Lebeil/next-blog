8c694f8b9fb7ff08eef65d45da9ab54157c3bbfa
Lebeil <liedel75@gmail.com>
2021-01-05 17:48:12 +0100

    add validators for auth

---
+++ backend/validators/auth.js
@@ -0,0 +1,14 @@
+const { check } = require('express-validator');
+
+exports.userSignupValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Veuillez remplir le nom !'),
+    check('email')
+        .isEmail()
+        .withMessage('Adresse mail incorrect !'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Mot de passe, minimum 6 caractères !!')
+];
