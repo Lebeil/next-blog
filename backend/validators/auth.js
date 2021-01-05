150cdfefe75854277c65e61b8eb1bd7ab6d44cb6
Lebeil <liedel75@gmail.com>
2021-01-05 20:59:51 +0100

    add signout & algorithms: ['sha1', 'RS256', 'HS256']

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -12,3 +12,12 @@ exports.userSignupValidator = [
         .isLength({ min: 6 })
         .withMessage('Mot de passe, minimum 6 caractères !!')
 ];
+
+exports.userSigninValidator = [
+    check('email')
+        .isEmail()
+        .withMessage('Adresse mail incorrect !'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Mot de passe, minimum 6 caractères !!')
+];
