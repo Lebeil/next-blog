efc632c454399c62a74ae57e6e467dc54a5e3424
Lebeil <liedel75@gmail.com>
2021-01-05 21:55:47 +0100

    Frontend + navbar + env

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,12 +1,14 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
     // console.log(req.body);
     User.findOne({ email: req.body.email }).exec((err, user) => {
         if (user) {
             return res.status(400).json({
-                error: 'Email est déjà pris !'
+                error: 'Email is taken'
             });
         }
 
@@ -21,12 +23,51 @@ exports.signup = (req, res) => {
                     error: err
                 });
             }
-            /* res.json({
-                 user: success
-             });*/
+            // res.json({
+            //     user: success
+            // });
             res.json({
-                message: 'Inscription ok ! Veuillez vous connecter'
+                message: 'Signup success! Please signin.'
             });
         });
     });
 };
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    // check if user exist
+    User.findOne({ email }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup.'
+            });
+        }
+        // authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'Email and password do not match.'
+            });
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { expiresIn: '1d' });
+        const { _id, username, name, email, role } = user;
+        return res.json({
+            token,
+            user: { _id, username, name, email, role }
+        });
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie('token');
+    res.json({
+        message: 'Déconnecté!'
+    });
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['sha1','RS256','HS256']
+});
---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Index = ()=> {
+    return(
+            <Layout>
+                <p>header</p>
+                <h2>Index page</h2>
+                <p>footer</p>
+            </Layout>
+        )
+}
+
+export default Index
